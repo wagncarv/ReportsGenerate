31b7be1c9e94394789690524e9e49f1f1fcfaa4b
Wagner Carvalho <wagncarv@gmail.com>
2021-03-08 14:59:56 -0300

    teste adicionado

---
+++ lib/sum_list.ex
@@ -0,0 +1,13 @@
+defmodule SumList do
+
+  def call(list), do: sum(list, 0)
+
+  defp sum([], acc), do: acc
+
+  defp sum([head | tail], acc) do
+    acc = acc + head
+    sum(tail, acc)
+  end
+end
+
+#SumList.sum
